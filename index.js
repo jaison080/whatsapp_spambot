d12526ec08f584fc00b5e0872e22a41501298dc4
jaison080 <jaisondennis080@gmail.com>
2022-08-28 10:33:35 +0530

    Inital commit

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-const puppeteer = require('puppeteer')
-let url = 'https://web.whatsapp.com/'
+const puppeteer = require("puppeteer");
+let url = "https://web.whatsapp.com/";
+
+let target = "Jagannath E Shahi"; //Contact Name
 
 const start = async (url) => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        args: ['--start-maximized'],
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.setDefaultTimeout(0);
+  await page.waitForSelector('[data-testid="search"]').then(() =>
+    page.click('[data-testid="search"]', {
+      delay: 3000,
     })
+  );
 
-    const page = await browser.newPage()
+  await page.type("._13NKt", target);
+  await page.keyboard.press("Enter");
 
-    await page.goto(url);
-    await page.setDefaultTimeout(0);
-    await page.waitForSelector('[data-testid="search"]')
-    .then(()=>  page.click('[data-testid="search"]', {
-      delay: 3000
-    }))
-   
-    await page.type("._13NKt", "Jagannath E Shahi");
-        await page.keyboard.press("Enter")
+  let input = await page.$(
+    "#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div"
+  );
 
-    let input = await page.$(
-        '#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div'
-    )
+  //Enter your custom messages here.....
+  let messages = [
+    "Spam",
+    "Its a spam attack",
+    "Have a great time",
+    "💚💚💚",
+    "🐱‍🚀🐱‍🚀🐱‍🚀",
+    "👾👾👾",
+    "🤖🤖🤖",
+    "🤡🤡🤡",
+  ];
 
-    let messages = [
-        'Spam',
-        'Its a spam attack',
-        'Have a great time',
-        '💚💚💚',
-        '🐱‍🚀🐱‍🚀🐱‍🚀',
-        '👾👾👾',
-        '🤖🤖🤖',
-        '🤡🤡🤡',
-    ]
+  for (let i = 0; i < 10; i++) {
+    let message = messages[Math.floor(Math.random() * messages.length)];
+    await input.type(message);
+    await page.keyboard.press("Enter");
+  }
 
-    for (let i = 0; i < 10; i++) {
-        let message = messages[Math.floor(Math.random() * messages.length)]
-        await input.type(message)
-        await page.keyboard.press('Enter')
-    }
-
-    // await browser.close()
-}
-start(url)
+  // await browser.close()
+};
+start(url);
